2cb4ac7d98e3ba9650412c1039faddda4622d983
Juliet-Simpson <simpsonjulietc@gmail.com>
2021-06-20 16:09:52 +0000

    Delete

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -14,11 +14,9 @@ connection = pymysql.connect(host='localhost',
 
 try:
     with connection.cursor() as cursor:
-        rows = [(23, 'bob'),
-                (24, 'jim'),
-                (25, 'fred')]
-        cursor.executemany("UPDATE Friends SET age = %s WHERE name = %s;",
-                           rows)
+        rows = cursor.execute("DELETE FROM Friends WHERE name = 'bob';")
+        connection.commit()
+
 finally:
     # Close the connection, regardless of whether or not the above was 
     # successful
